c34e8fb02f622dd74bd183a1e7a06e1f87b5f96f
thegarrettscott <78319808+thegarrettscott@users.noreply.github.com>
2022-10-15 14:58:59 -0500

    Add files via upload

---
+++ bettersearch3/googleDocsCanvasInjector.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e=document.createElement("script");e.src=chrome.runtime.getURL("content/gDocsAnnotatedCanvas.js"),(document.head||document.documentElement).appendChild(e)})();
